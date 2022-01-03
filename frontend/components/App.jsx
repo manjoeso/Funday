bdaacd55fe2b1d88cdd982762636e5f198e6ec4d
manjoeso <jmanso93@gmail.com>
2022-01-02 20:15:05 -0500

    have routing set up but version is too new, downgrading react-outer-dom

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,10 +5,14 @@ import SplashContainer from './splash/splash_container'
 
 import LoginFormContainer from './sessions/login_form_container'
 import SignupFormContainer from './sessions/sign_up_form_container'
+import { AuthRoute } from '../util/route_util';
 
 
 const App = () => (
   <div>
+    <header>
+      <Link to='/' className='header-link'/>>
+    </header>
     <h1>App is working</h1>
       <Routes>
         <Route path="/" element={<SplashContainer/>} />
--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Route, Redirect, withRouter } from 'react-router-dom';
+import { Route, Navigate, withRouter } from 'react-router-dom';
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
     <Route
